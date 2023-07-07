75c0b4052c7083a0169a5d88a0c598ec09ebad36
Asad Mehboob <omarpuengg@gmail.com>
2023-07-07 12:25:23 +0500

    gallery using flexbox and before after

---
+++ image-gallery-flex-profile/js/script.js
@@ -0,0 +1,32 @@
+$('.profile').bind('click', function() {
+	
+	$('.detail').addClass('active');
+});
+
+$('.close').bind('click', function() {
+	$('.detail').removeClass('active');
+});
+
+
+const menuIconEl = $('.menu-icon');
+    const sidenavEl = $('.sidenav');
+    const sidenavCloseEl = $('.sidenav__close-icon');
+
+    // Add and remove provided class names
+    function toggleClassName(el, className) {
+      if (el.hasClass(className)) {
+        el.removeClass(className);
+      } else {
+        el.addClass(className);
+      }
+    }
+
+    // Open the side nav on click
+    menuIconEl.on('click', function() {
+      toggleClassName(sidenavEl, 'active');
+    });
+
+    // Close the side nav on click
+    sidenavCloseEl.on('click', function() {
+      toggleClassName(sidenavEl, 'active');
+    });
